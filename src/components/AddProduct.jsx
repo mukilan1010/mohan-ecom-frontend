6728d6abcf2a0ac9964605da39915b026bc48d1b
MOHANRAJ B <122554268+Mohan-1708@users.noreply.github.com>
2025-08-30 20:10:41 +0530

    Initial commit - frontend project

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -34,7 +34,7 @@ const AddProduct = () => {
     );
 
     axios
-        .post("http://localhost:8080/api/product", formData, {
+        .post("http://13.48.29.25:8080/api/product", formData, {
           headers: {
             "Content-Type": "multipart/form-data",
           },
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = ({ onSelectCategory, onSearch }) => {
 
   const fetchData = async (value) => {
     try {
-      const response = await axios.get("http://localhost:8080/api/products");
+      const response = await axios.get("http://13.48.29.25:8080/api/products");
       setSearchResults(response.data);
       console.log(response.data);
     } catch (error) {
@@ -36,7 +36,7 @@ const Navbar = ({ onSelectCategory, onSearch }) => {
       setShowSearchResults(true)
       try {
         const response = await axios.get(
-            `http://localhost:8080/api/products/search?keyword=${value}`
+            `http://13.48.29.25:8080/api/products/search?keyword=${value}`
         );
         setSearchResults(response.data);
         setNoResults(response.data.length === 0);
